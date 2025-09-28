7eee5f98cbbb56f648b3ac74ddbe79ba65c7646a
Hassankex <hassankex432@gmail.com>
2025-09-28 17:20:52 +0100

    Print hello

---
+++ main.py
@@ -0,0 +1,14 @@
+#print a sentence
+print("hello everyone")
+
+
+#print a number
+print(10)
+
+
+#print both numbers and text
+print("hassan",12)
+
+
+#print an empty line before/after a sentence
+print("\n i am penguin")
