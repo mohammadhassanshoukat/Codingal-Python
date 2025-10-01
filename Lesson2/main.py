4951f43ff7d2731a0f1b700f9b80376e8eb8db30
Hassankex <fariha_es@yahoo.com>
2025-10-01 19:13:42 +0100

    lesson 2

---
+++ Lesson2/main.py
@@ -0,0 +1,13 @@
+#print a sentence
+print("Hello")
+
+
+#print a number
+print(42)
+
+#next line after print
+print("\nhello world\n\n")
+
+
+#passmultiple arguments for printting
+print("hello","world",42)
---
+++ Lesson2/main2.py
@@ -0,0 +1,16 @@
+#creating different variables
+x = 5
+y = "Hello, World!"
+print(x)
+print
+
+
+condingal = "CODINGAL"
+print(condingal)
+
+
+
+#input user data and store in a variable
+name = input("enteryur name: ")
+
+print ("hello", name," welcome to codingal")
