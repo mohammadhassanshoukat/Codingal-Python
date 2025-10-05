1c7a1f8e9aa228c680440831cdc256b0c074d47e
Hassankex <fariha_es@yahoo.com>
2025-10-05 18:52:14 +0100

    acp

---
+++ operators  1 l4/acp.py
@@ -0,0 +1,7 @@
+import math
+
+# Calculate the square root of the number
+number = int(input("Enter a number to find its square root: "))
+square_root = math.sqrt(number)
+print(f"The square root of {number} is {square_root}")
+
