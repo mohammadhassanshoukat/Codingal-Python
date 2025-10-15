18961d1cf1d20bb9910b6c8f866d739874a0782e
Hassankex <fariha_es@yahoo.com>
2025-10-15 19:08:06 +0100

    class projects

---
+++ conditional nested statement/acp.py
@@ -0,0 +1,11 @@
+#take the input of the attendance
+age=int(input("Enter your age : "))
+
+
+
+#checking the user input predecting accordingaly
+if age<=20:
+    print("You are allowed to come in the class to come in the class ")
+else:
+        print("you are not allowed to come in the class ")
+    
