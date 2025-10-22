702626ca232e584987ba7cd105a80019a88a192f
Hassankex <fariha_es@yahoo.com>
2025-10-22 19:32:32 +0100

    python

---
+++ half pyramid l10/acp.py
@@ -0,0 +1,11 @@
+# take input
+print("Full mirrored triangle pattern of stars (*):")
+n = int(input("Enter the number of rows: "))
+
+
+for i in range(n):
+    for j in range(n - i - 1):
+        print(" ", end="")
+    for j in range(2 * i + 1):
+        print("*", end="")
+    print()
