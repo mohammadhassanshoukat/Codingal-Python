b9f9f6bc4de9848f9eefb3a2e4521a5ade07f978
Hassankex <fariha_es@yahoo.com>
2025-11-12 18:10:49 +0000

    python

---
+++ l16/acp.py
@@ -0,0 +1,6 @@
+try:
+    age = int(input("Enter your age: "))
+    if age % 2 == 0:
+        print("The age is even.")
+    else:
+        print("The age is odd.")
---
+++ l16/acp1.py
@@ -0,0 +1,9 @@
+import math
+
+angle = float(input("Enter angle in degrees: "))
+
+radian = math.radians(angle)
+
+print("sin(", angle, ") =", math.sin(radian))
+print("cos(", angle, ") =", math.cos(radian))
+print("tan(", angle, ") =", math.tan(radian))
