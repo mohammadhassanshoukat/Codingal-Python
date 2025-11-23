73e4202df1a00f79fab01a795a68e0c25c286be5
Hassankex <fariha_es@yahoo.com>
2025-11-23 16:48:19 +0000

    python

---
+++ l21/intersection.py
@@ -0,0 +1,9 @@
+setc1={"green","blue"}
+setc2={"blue","yellow"}
+print("Orignal Set")
+print(setc1)
+print(setc2)
+
+print("\nIntersection of both sets: ")
+setz=setc1.intersection(setc2)
+print(setz)
